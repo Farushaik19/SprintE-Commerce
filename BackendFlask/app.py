a02f75b4e198a4625dff22fdf13c66d35974f184
Aayushi Singh <53362464+Aayushii-Siingh@users.noreply.github.com>
2024-05-27 12:41:07 +0530

    Added User Logout Functionality

--- BackendFlask/app.py
+++ BackendFlask/app.py
@@ -11,6 +11,16 @@ cursor.execute("SHOW DATABASES")
 for databases in cursor:
     print(databases)
 
+
+#USER LOGOUT
+@app.route('/logout', methods=['GET','POST'])
+def logout():
+    # session.clear()
+    return jsonify({'message': 'You have been logged out.'}), 200
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
 # Closing the cursor and connection to the database
 cursor.close()
 db.close();
