0e09d0db8284e809769a77ab2a784fdc701e0265
Aayushi Singh <0523aayushi@gmail.com>
2024-05-28 14:10:16 +0530

    implemented CORS and Session_data code

--- BackendFlask/app.py
+++ BackendFlask/app.py
@@ -2,24 +2,94 @@ from database import db
 from products import Product
 from carts import Cart
 import secrets
-import pymysql
-from flask import Flask, jsonify, request, session, redirect, url_for, flash
+from flask import Flask, jsonify, request, session, redirect, url_for, flash, render_template
 from flask_bcrypt import Bcrypt
 import re
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app, supports_credentials=True)
 
 secret_key = secrets.token_hex(16)
 app.secret_key = secret_key
 
+bcrypt = Bcrypt(app)
+
+# Email validation function
+def is_valid_email(email):
+    regex = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
+    return re.match(regex, email)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/signup', methods=['POST'])
+def signup():
+    email = request.json.get('email')
+    password = request.json.get('password')
+    is_admin = 0
+
+    if not is_valid_email(email):
+        return jsonify({'message': 'Invalid email format. Please enter a valid email address.'}), 400
+
+    cur = db.cursor()
+    cur.execute("SELECT * FROM users WHERE email = %s", (email,))
+    existing_user = cur.fetchone()
+
+    if existing_user:
+        return jsonify({'message': 'Email already exists. Please choose a different email.'}), 400
+    else:
+        password_hash = bcrypt.generate_password_hash(password).decode('utf-8')
+        cur.execute("INSERT INTO users (email, password_hash, is_admin) VALUES (%s, %s, %s)", (email, password_hash, is_admin))
+        db.commit()
+        cur.close()
+        return redirect(url_for('home'))
+
+@app.route('/login', methods=['POST'])
+def login():
+    email = request.json.get('email')
+    password = request.json.get('password')
+
+    if not is_valid_email(email):
+        return jsonify({'message': 'Invalid email format. Please enter a valid email address.'}), 400
+
+    cur = db.cursor()
+    cur.execute("SELECT * FROM users WHERE email = %s", (email,))
+    user = cur.fetchone()
+    cur.close()
+
+    if user and bcrypt.check_password_hash(user[2], password):
+        session['user_id'] = user[0]
+        session['email'] = user[1]
+        session['is_admin'] = user[3]
+        return jsonify({'message': 'Login successful', 'user_id': user[0], 'email': user[1], 'is_admin': user[3]})
+    else:
+        return jsonify({'message': 'Login unsuccessful. Please check email and password.'}), 401
+
+@app.route('/logout', methods=['POST'])
+def logout():
+    session.clear()
+    return jsonify({'message': 'You have been logged out.'}), 200
+
+@app.route('/session_data', methods=['GET'])
+def session_data():
+    if 'user_id' in session:
+        return jsonify({
+            'user_id': session['user_id'],
+            'email': session['email'],
+            'is_admin' : session['is_admin']
+        }), 200
+    else:
+        return jsonify({'message': 'No active session found.'}), 401
+
 @app.route('/addProduct', methods=['POST'])
 def add_product():
-    # Extract product data from the request
-    data = request.json
+    data = request.form
     name = data.get('name')
     description = data.get('description')
     price = data.get('price')
-    # Check if all required fields are present
+
     if not (name and description and price):
         return jsonify({'message': 'Missing required fields'}), 400
 
@@ -38,18 +108,11 @@ def getProducts():
     except Exception as e:
         return jsonify({"message": "Error occurred", "error": str(e)}), 500
 
-
-
-#Add to Cart
 @app.route('/addCart', methods=['POST'])
 def add_to_cart():
-    # Extract product data from the request
-
-    data = request.get_json()
-
+    data = request.form
     prod_id = data.get('product_id')
     user_id = data.get('user_id')
-    # Check if all required fields are present
 
     if not (prod_id and user_id):
         return jsonify({'message': 'Missing required fields'}), 400
@@ -69,92 +132,5 @@ def getUserCart(user_id):
     except Exception as e:
         return jsonify({"message": "Error occurred", "error": str(e)}), 500
 
-#USER REGISTRATION CODE
-
-# Initialize Flask-Bcrypt
-bcrypt = Bcrypt(app)
-
-# Email validation function
-def is_valid_email(email):
-    # Regular expression for validating an email
-    regex = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
-    return re.match(regex, email)
-
-#USER LOGIN
-@app.route('/login', methods=['POST'])
-def login():
-    email = request.json.get('email')
-    password = request.json.get('password')
-
-    if not is_valid_email(email):
-        return jsonify({'message': 'Invalid email format. Please enter a valid email address.'}), 400
-
-    cur = db.cursor()
-    cur.execute("SELECT * FROM users WHERE email = %s", (email,))
-    user = cur.fetchone()
-    cur.close()
-
-#user[0] is likely the user ID, user[1] is the user's email, and user[2] is the hashed password. 
-    if user and bcrypt.check_password_hash(user[2], password):
-        session['user_id'] = user[0]
-        session['email'] = user[1]
-        session['is_admin'] = user[3]
-        return jsonify({'message': 'Login successful', 'user_id': user[0], 'email': user[1]})
-    else:
-        return jsonify({'message': 'Login unsuccessful. Please check email and password.'}), 401
-
-#USER SIGNUP
-@app.route('/signup', methods=['POST'])
-def signup():
-    email = request.json.get('email')
-    password = request.json.get('password')
-    print("data got is ",request.json)
-
-    if not is_valid_email(email):
-        return jsonify({'message': 'Invalid email format. Please enter a valid email address.'}), 400
-
-    cur = db.cursor()
-    cur.execute("SELECT * FROM users WHERE email = %s", (email,))
-    existing_user = cur.fetchone()
-
-    if existing_user:
-        return jsonify({'message': 'Email already exists. Please choose a different email.'}), 400
-    else:
-        password_hash = bcrypt.generate_password_hash(password).decode('utf-8')
-        cur.execute("INSERT INTO users (email, password_hash) VALUES (%s, %s)", (email, password_hash))
-        db.commit()
-        cur.close()
-        return jsonify({'message': 'User registered successfully'}), 201
-
-#USER FORGOT PASSWORD
-@app.route('/forgot_password', methods=['POST'])
-def forgot_password():
-    email = request.json.get('email')
-    new_password = request.json.get('new_password')
-
-    if not is_valid_email(email):
-        return jsonify({'message': 'Invalid email format. Please enter a valid email address.'}), 400
-
-
-    cur = db.cursor()
-    cur.execute("SELECT * FROM users WHERE email = %s", (email,))
-    user = cur.fetchone()
-
-    if user:
-        password_hash = bcrypt.generate_password_hash(new_password).decode('utf-8')
-        cur.execute("UPDATE users SET password_hash = %s WHERE email = %s", (password_hash, email))
-        db.commit()
-        cur.close()
-        return jsonify({'message': 'Password updated successfully.'}), 200
-    else:
-        return jsonify({'message': 'Email not found. Please enter a valid email address.'}), 404
-
-#USER LOGOUT
-@app.route('/logout', methods=['POST'])
-def logout():
-    session.clear()
-    return jsonify({'message': 'You have been logged out.'}), 200
-
 if __name__ == '__main__':
     app.run()
-
