26ad9de035c8be557f395019042bf077878453cd
Aayushi Singh <53362464+Aayushii-Siingh@users.noreply.github.com>
2024-05-28 09:12:41 +0530

    Delete BackendFlask/logout.py

--- BackendFlask/logout.py
+++
@@ -1,8 +0,0 @@
-
-#USER LOGOUT
-@app.route('/logout', methods=['GET','POST'])
-def logout():
-    return jsonify({'message': 'You have been logged out.'}), 200
-
-if __name__ == '__main__':
-    app.run(debug=True)
