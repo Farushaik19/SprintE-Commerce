10617dc31c48cbe2c83d4575bc0ddb78864ae039
ShohaibKhan <khanshohaib400@gmail.com>
2024-05-28 15:30:08 +0530

    deletion is and product image is done

--- BackendFlask/app.py
+++ BackendFlask/app.py
@@ -6,9 +6,11 @@ import pymysql
 from flask import Flask, jsonify, request, session, redirect, url_for, flash
 from flask_bcrypt import Bcrypt
 import re
+import requests
+import random
 
 app = Flask(__name__)
-
+frontEnd = "http://127.0.0.1:8000/"
 secret_key = secrets.token_hex(16)
 app.secret_key = secret_key
 
@@ -20,11 +22,23 @@ def add_product():
     description = data.get('description')
     price = data.get('price')
     # Check if all required fields are present
+    image_url = "https://images.unsplash.com/photo-1511556820780-d912e42b4980?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w2MTY4NjB8MHwxfHNlYXJjaHwxfHxwcm9kdWN0c3xlbnwwfHx8fDE3MTY4ODgyMTR8MA&ixlib=rb-4.0.3&q=80&w=1080"
+    try:
+        for i in range(2):
+            response = requests.get(
+                "https://api.unsplash.com/search/photos/?query=products&per_page=10&client_id=hgREOQ-PsXIYeyLu61Eu08GpwxvNhA2k8mlpWyBhiXU")
+            data = response.json()
+            image = random.choice(data['results'])
+            image_url = image['urls']['regular']
+
+    except Exception as e:
+        print('No images found.', str(e))
+
     if not (name and description and price):
         return jsonify({'message': 'Missing required fields'}), 400
 
     try:
-        product= Product(name,description,price)
+        product= Product(name,description,price,image_url)
         product.insertProduct()
         return jsonify({'message': 'Product added successfully','product_id':product.product_id}), 201
     except Exception as e:
@@ -76,7 +90,7 @@ def getUserCartProducts(user_id):
         for cart in userCart:
             cartProds.append(Product.getProductbyId(cart[2]))
 
-        return jsonify({"message":"Success","cart":cartProds}),200
+        return jsonify({"message":"Success","cart":cartProds,"userId":user_id}),200
     except Exception as e:
         return jsonify({"message": "Error occurred", "error": str(e)}), 500
 
@@ -167,6 +181,26 @@ def forgot_password():
 def logout():
     session.clear()
     return jsonify({'message': 'You have been logged out.'}), 200
+
+# deletion of product from cart
+@app.route('/removeProduct/<int:prod_id>/<int:uid>', methods=['POST'])
+def removeProduct(prod_id,uid):
+    try:
+        Cart.removeProdUser(prod_id,uid)
+        return redirect(frontEnd+"cart")
+    except Exception as e:
+        print(str(e))
+        return jsonify({'error': 'something went wrong during deletion'})
+
+@app.route('/deleteProduct/<int:prod_id>', methods=['POST'])
+def deleteProduct(prod_id):
+    try:
+        Product.deleteProduct(prod_id)
+        return redirect(frontEnd)
+    except Exception as e:
+        print(str(e))
+        return jsonify({'error': 'something went wrong during deletion'})
+
 
 
 
